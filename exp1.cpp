b6ca81d57830e533fd40c9a1d6a3d7168a05cbbd
vvkrghv <raghavvivek2005@gmail.com>
2026-01-12 16:26:34 +0530

    Update exp1.cpp

--- exp1.cpp
+++ exp1.cpp
@@ -2,55 +2,70 @@
 using namespace std;
 using namespace std::chrono;
 
-
-void complexRec(int n) {
-
-
-   if (n <= 2) {
-       return;
-   }
+long long operations = 0;
+int maxDepth = 0;
 
 
-   int p = n;
-   while (p > 0) {
-       vector<int> temp(n);
-       for (int i = 0; i < n; i++) {
-           temp[i] = i ^ p;
-       }
-       p >>= 1;
-   }
+void complexRec(int n, int depth) {
 
+    operations++; // function call
+    maxDepth = max(maxDepth, depth);
 
-   vector<int> small(n);
-   for (int i = 0; i < n; i++) {
-       small[i] = i * i;
-   }
+    if (n <= 2) {
+        return;
+    }
 
+    int p = n;
+    while (p > 0) {
+        operations++;
+        vector<int> temp(n);
+        for (int i = 0; i < n; i++) {
+            operations++;
+            temp[i] = i ^ p;
+        }
+        p >>= 1;
+    }
 
-   if (n % 3 == 0) {
-       reverse(small.begin(), small.end());
-   } else {
-       reverse(small.begin(), small.end());
-   }
-   
-   // Recurrence Relation : T(n)=2 + n+ nlogn+ 3T(n/2)
-   
-   // Time complexity : O((n^log(3) base 2))
-   auto start = high_resolution_clock::now();
+    vector<int> small(n);
+    for (int i = 0; i < n; i++) {
+        operations++;
+        small[i] = i * i;
+    }
 
-   complexRec(n / 2);
-   complexRec(n / 2);
-   complexRec(n / 2);
+    operations++;
+    reverse(small.begin(), small.end());
 
-   auto end = high_resolution_clock::now();
-   auto duration = duration_cast<milliseconds>(end - start);
-   cout << duration.count() << endl;
+    // Recursive calls
+    complexRec(n / 2, depth + 1);
+    complexRec(n / 3, depth + 1);
+
+    //recurrence relation = 2+n+nlogn+2T(n/2)
+    //time complexity = O(n^log(3) base 2)
 }
-int main()
-{
-    complexRec(20);
+
+int main() {
+
+    vector<int> testSizes = {10, 20, 40, 80};
+
+    cout << "n\tOperations\tMax Depth\tTime (ms)\n";
+    cout << "---------------------------------------------\n";
+
+    for (int n : testSizes) {
+
+        operations = 0;
+        maxDepth = 0;
+
+        auto start = high_resolution_clock::now();
+        complexRec(n, 1);
+        auto end = high_resolution_clock::now();
+
+        auto duration = duration_cast<milliseconds>(end - start);
+
+        cout << n << "\t"
+             << operations << "\t\t"
+             << maxDepth << "\t\t"
+             << duration.count() << endl;
+    }
+
     return 0;
 }
-
-
-
